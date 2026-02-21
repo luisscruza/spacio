ba73776ecddbe92c876dfc7add0491a7e081d56b
Luis De la Cruz <98129640+cruzmediaorg@users.noreply.github.com>
2026-02-21 11:48:38 +0100

    init session

--- public/index.php
+++ public/index.php
@@ -2,10 +2,13 @@
 
 use Spacio\Framework\Http\Kernel;
 use Spacio\Framework\Http\Request;
+use Spacio\Framework\Http\Session;
 
 define('BASE_PATH', dirname(__DIR__));
 
 require_once BASE_PATH.'/vendor/autoload.php';
+
+Session::start();
 
 $container = require BASE_PATH.'/bootstrap/app.php';
 
